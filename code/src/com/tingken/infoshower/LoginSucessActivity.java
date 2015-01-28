d3919cdeaa308c1f2c0c6d9cbad6a7162035c125
tingken <hanqiankun@gmail.com>
2015-01-29 02:13:28 +0800

    add two views

---
+++ code/src/com/tingken/infoshower/LoginSucessActivity.java
@@ -0,0 +1,17 @@
+package com.tingken.infoshower;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.Window;
+
+public class LoginSucessActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		setContentView(R.layout.activity_login_sucess);
+	}
+}
